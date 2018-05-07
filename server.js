80c6014522f5286f1f32cb8edd415acad1a73233
Putu Harum Bawa <lightharum.108@gmail.com>
2018-05-08 00:42:37 +0700

    read post body

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 var restify = require('restify');
 
-const mockup_res = {
-  name: 'Putu Harum Bawa',
-  age: 24,
-};
-
 const user_portofolio = {
   "status_code": 200,
   "data": [
     {
-      "id": 130671,
-      "product_code": "IDN000074903",
-      "product_name": "Mandiri Investa Dana Syariah",
-      "im_code": "CC002",
-      "im_name": "Mandiri Manajemen Investasi, PT",
-      "initial_average_nav": "3,375.38",
-      "initial_investment": "500,000.00",
-      "nett_unit": "148.1315",
-      "last_nav": "3,360.7400",
-      "current_investment_value": "497,831.36",
+      "id": 101749,
+      "product_code": "IDN000265709",
+      "product_name": "CIMB-PRINCIPAL Bukareksa Pasar Uang",
+      "im_code": "ZG002",
+      "im_name": "CIMB Principal Asset Management, PT",
+      "initial_average_nav": "1,059.04",
+      "initial_investment": "100,000.00",
+      "nett_unit": "94.4254",
+      "last_nav": "1,064.1000",
+      "current_investment_value": "100,478.04",
     },
     {
       "id": 131021,
@@ -32,22 +27,17 @@ const user_portofolio = {
       "last_nav": "1,054.5424",
       "current_investment_value": "500,437.02",
     },
-    {
-      "id": 131980,
-      "product_code": "IDN000019205",
-      "product_name": "BNP Paribas Pesona",
-      "im_code": "MEES2",
-      "im_name": "BNP Paribas Investment Partners, PT",
-      "initial_average_nav": "26,485.78",
-      "initial_investment": "1,000,000.00",
-      "nett_unit": "37.7561",
-      "last_nav": "25,901.5400",
-      "current_investment_value": "977,941.37",
-    }
   ]
 }
 
-var server = restify.createServer();
+const server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
 
 server.get('/user/portfolio', function(req, res, next) {
   res.send(user_portofolio);
@@ -57,7 +47,7 @@ server.get('/user/portfolio', function(req, res, next) {
 server.post('/order/submitOrder', function(req, res, next) {
   res.send({
     status_code: 200,
-    order_no: 1000000
+    order_no: req.body.bl_id
   });
   next();
 })
